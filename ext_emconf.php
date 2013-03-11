167f362a44f25d3e08ddae25f728bd142f9a5028
Kay Strobach <github@kay-strobach.de>
2013-03-11 20:32:15 +0100

    TER Release

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "basictemplate".
-#
-# Auto generated 12-08-2011 20:21
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "basictemplate".
+ *
+ * Auto generated 11-03-2013 19:39
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Easy frontend Template done with Fluid',
 	'description' => 'This is a minimal skin configured for skin selector!',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.0.4',
+	'version' => '1.0.5',
 	'dependencies' => 'cms,extbase,fluid',
 	'conflicts' => '',
 	'priority' => '',
@@ -32,19 +32,24 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'private',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'cms' => '',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'cms' => '4.5.0-6.0.99',
 			'extbase' => '',
 			'fluid' => '',
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
 		),
-		'suggests' => array(
+		'suggests' => 
+		array (
 		),
 	),
 	'_md5_values_when_last_written' => 'a:22:{s:21:"ext_conf_template.txt";s:4:"00d0";s:12:"ext_icon.gif";s:4:"6759";s:17:"ext_localconf.php";s:4:"b509";s:14:"ext_tables.php";s:4:"1764";s:45:"Classes/ViewHelpers/Format/ShowViewHelper.php";s:4:"968f";s:38:"Configuration/PageTS/backendlayout.txt";s:4:"fcf1";s:38:"Configuration/TypoScript/constants.txt";s:4:"c03f";s:34:"Configuration/TypoScript/setup.txt";s:4:"122e";s:40:"Resources/Private/Language/locallang.xml";s:4:"411a";s:45:"Resources/Public/Images/BackendScreenshot.png";s:4:"d7da";s:52:"Resources/Public/JavaScripts/IeHtml5Compatibility.js";s:4:"10ed";s:37:"Resources/Public/Layouts/Default.html";s:4:"a3af";s:48:"Resources/Public/Partials/nocontent-message.html";s:4:"70de";s:40:"Resources/Public/Partials/nocontent.html";s:4:"717c";s:44:"Resources/Public/Partials/typo3-message.html";s:4:"8afd";s:40:"Resources/Public/Stylesheets/article.css";s:4:"9ad8";s:38:"Resources/Public/Stylesheets/aside.css";s:4:"6070";s:39:"Resources/Public/Stylesheets/footer.css";s:4:"5a14";s:39:"Resources/Public/Stylesheets/header.css";s:4:"d41d";s:37:"Resources/Public/Stylesheets/main.css";s:4:"d4ec";s:36:"Resources/Public/Stylesheets/nav.css";s:4:"6202";s:36:"Resources/Public/Templates/main.html";s:4:"995c";}',
-	'suggests' => array(
+	'suggests' => 
+	array (
 	),
 );
 
